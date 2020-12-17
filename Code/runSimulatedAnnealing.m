51fc6676b37810f1dba70127daf3806f872904b5
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-12-17 16:49:18 +0900

    update output saving structure in SA

--- Code/runSimulatedAnnealing.m
+++ Code/runSimulatedAnnealing.m
@@ -13,8 +13,14 @@ function [s_set, HS, more_output] = runSimulatedAnnealing(costfun, N, opts)
     quenchOptions = opts.quench;
     
     % optional settings
-    save_output = getFromStruct(opts, 'save_output', false);
     talkative = getFromStruct(opts, 'talkative', true);
+    save_output = getFromStruct(opts, 'save_output', false);
+    if ~isfield(opts, 'overwrite')
+        opts.overwrite = false;
+    end
+    
+    more_output = [];
+    more_output.term_message = ''; % empty means normal
 
 
     % ==== initialization
@@ -22,22 +28,25 @@ function [s_set, HS, more_output] = runSimulatedAnnealing(costfun, N, opts)
     const_a = getFromStruct(saOptions, 'const_a', 0.5);
     T_init_range = getFromStruct(saOptions, 'T_init_range', []);
     
-    [s_init, ~] = initial_state_generation(N);
-    T_init = initial_temp_decision(costfun, s_init, const_a, T_init_range);
+    [s_set, ~] = initial_state_generation(N);
+    T = initial_temp_decision(costfun, s_set, const_a, T_init_range);
     if talkative
-        disp(['Initial T=',num2str(T_init,'%1.1f')]);
+        disp(['Initial T=',num2str(T,'%1.1f')]);
     end
     if save_output
         % save initial information
-        save_initial_info(opts, s_init, costfun(s_init), T_init)
+        HS = costfun(s_set);
+        flag = save_initial_info(opts, s_set, HS, T);
+        if flag
+            msg = 'failed to save initial_info';
+            disp(msg);
+            more_output.term_message = msg;
+            return;
+        end
     end
 
 
     % ==== simulated annealing
-
-    % initialize
-    T = T_init;
-    s_set = s_init;
 
     c_cool = saOptions.c_cool;
     maxIterSA = saOptions.maxIterSA;
@@ -60,6 +69,7 @@ function [s_set, HS, more_output] = runSimulatedAnnealing(costfun, N, opts)
             disp(' - premature end of MCMC. maxIter reached.')
         end
         quit_cond_SA = (term_status_MCMC<0);
+        more_output.samp_term_status = term_status_MCMC;
 
         % choose best sample (cost minimizer)
         [HS,ipick] = min(HS_list);
@@ -71,7 +81,13 @@ function [s_set, HS, more_output] = runSimulatedAnnealing(costfun, N, opts)
 
         if (save_output && rem(iter,20) == 0)
             % save sampling output from this T
-            save_samp_output_fixedT(opts, iter, HS_list, s_set, t_elap, T)
+            flag = save_samp_output_fixedT(opts, iter, HS_list, s_set, t_elap, T);
+            if flag
+                msg = ['failed to save sampling output at T=', num2str(T)];
+                disp(msg);
+                more_output.term_message = msg;
+                return;
+            end
         end
 
         if talkative
@@ -81,10 +97,12 @@ function [s_set, HS, more_output] = runSimulatedAnnealing(costfun, N, opts)
         end
 
         if quit_cond_SA
+            msg = '* single-domain solution reached -- force quitting SA.';
             if talkative
-                disp('* single-domain solution reached -- force quitting SA.');
+                disp(msg);
             end
-            break
+            more_output.term_message = msg;
+            break;
         end
 
         % update temperature
@@ -105,29 +123,45 @@ function [s_set, HS, more_output] = runSimulatedAnnealing(costfun, N, opts)
     % zero temperature quenching (gradient descent)
     tic;
     [s_set,HS,HS_list,term_status] = runQuench_zeroT(costfun,s_set,quenchOptions);
-    if talkative
-        if (term_status==1)
+    t_elap = toc;
+    t_elap_total = t_elap_total + t_elap;
+    
+    if (term_status==1)
+        if talkative
             disp(' - bad sign: descending too far. trial limit reached.');
-            %warning('maybe SA was not enough. check temperature schedule.');
-        elseif (term_status==2)
+        end
+        %warning('maybe SA was not enough. check temperature schedule.');
+    elseif (term_status==2)
+        if talkative
             disp('Minimum reached successfully.');
         end
     end
-
-    t_elap = toc;
-    t_elap_total = t_elap_total + t_elap;
+    more_output.quench_term_status = term_status;
+    
     if save_output
-        save_quench_output(opts, HS_list, s_set, t_elap_total);
+        flag = save_quench_output(opts, HS_list, s_set, t_elap_total);
+        if flag
+            msg = 'failed to save quench output';
+            disp(msg);
+            more_output.term_message = msg;
+            return;
+        end
     end
-
-    more_output = struct('HS_list', HS_list);
+    
+    more_output.HS_list = HS_list;
 
 end
 
-function save_initial_info(opts, s_init, H_init, T_init)
+function flag = save_initial_info(opts, s_init, H_init, T_init)
     % save initial information
     save_folder_name = opts.out_path;
-    save_name = [save_folder_name, '/initial_info.mat'];
+    % save_name = fullfile(save_folder_name, 'initial_info.mat');
+    save_name = fullfile(save_folder_name, 'SA_init.mat');
+    if ~opts.overwrite && exist(save_name, 'file')
+        warning(['file already exists: ', save_name]);
+        flag = 1;
+        return
+    end
 
     % match legacy namespace
     % eval(['save ',savename,' samplingtype r s_set HS_sum initial_temp comb_ratio '])
@@ -138,12 +172,19 @@ function save_initial_info(opts, s_init, H_init, T_init)
     initial_info.initial_temp = T_init;
     initial_info.comb_ratio = 0.5; % no longer used
     save(save_name, '-struct', 'initial_info');
+    flag = 0;
 
 end
 
-function save_samp_output_fixedT(opts, iter, HS_list, s_set, t_elap, T)
+function flag = save_samp_output_fixedT(opts, iter, HS_list, s_set, t_elap, T)
     save_folder_name = opts.out_path;
-    save_name = [save_folder_name, '/results', num2str(iter), '.mat'];
+    % save_name = fullfile(save_folder_name, ['results', num2str(iter), '.mat']);
+    save_name = fullfile(save_folder_name, ['SA_it', num2str(iter, '%03d'), '.mat']);
+    if ~opts.overwrite && exist(save_name, 'file')
+        warning(['file already exists: ', save_name]);
+        flag = 1;
+        return
+    end
 
     % match legacy namespace
     % eval(['save ',save_name,' HS_list s_set t_elap tmp'])
@@ -153,20 +194,28 @@ function save_samp_output_fixedT(opts, iter, HS_list, s_set, t_elap, T)
     output.t_elap = t_elap;
     output.tmp = T;
     save(save_name, '-struct', 'output');
+    flag = 0;
 
 end
 
-function save_quench_output(opts, HS_list, s_set, t_elap_total)
+function flag = save_quench_output(opts, HS_list, s_set, t_elap_total)
     % NOTE: also save every 1000 trials as /ZeroT# ?
     save_folder_name = opts.out_path;
-    save_name=[save_folder_name,'/ZeroTfinal'];
+    % save_name = fullfile(save_folder_name, 'ZeroTfinal.mat');
+    save_name = fullfile(save_folder_name, 'SA_final.mat');
+    if ~opts.overwrite && exist(save_name, 'file')
+        warning(['file already exists: ', save_name]);
+        flag = 1;
+        return
+    end
 
     % match legacy namespace
     % eval(['save ',save_name,' s_set time_cost HS_list']);
     output = [];
     output.s_set = s_set;
-    output.HS_lisit = HS_list;
+    output.HS_list = HS_list;
     output.time_cost = t_elap_total;
     save(save_name, '-struct', 'output');
+    flag = 0;
 
 end
